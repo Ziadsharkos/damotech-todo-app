0508d09d4393990e67ab8a70c3affcca6636de84
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-10-01 19:40:24 -0400

    chore: configure AngularFire providers in app config

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBg0PWTZKxZXR49kxQF71BcA8zwWM7YhLQ",
+    authDomain: "damotech-todo-app.firebaseapp.com",
+    projectId: "damotech-todo-app",
+    storageBucket: "damotech-todo-app.firebasestorage.app",
+    messagingSenderId: "631668367053",
+    appId: "1:631668367053:web:80faaf954b38ef1c1b1384"
+  }
+};
