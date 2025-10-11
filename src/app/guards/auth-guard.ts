31d221034986135933dfb1f957d85363ea4a0191
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-10-11 19:09:01 -0400

    fix(auth): complete auth guard implementation

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+import { map, take } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.user$.pipe(
+    take(1),
+    map(user => {
+      if (user) {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    })
+  );
 };
