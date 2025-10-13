f217bafff93c6c1a024e8a50aa59db5c36a2c7c4
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-10-13 18:29:10 -0400

    feat(scaffold): generate ui/shared/auth/todos/page components with proper folders

---
+++ src/app/components/features/todos/todo-filters/todo-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-filters',
+  imports: [],
+  templateUrl: './todo-filters.html',
+  styleUrl: './todo-filters.scss'
+})
+export class TodoFilters {
+
+}
