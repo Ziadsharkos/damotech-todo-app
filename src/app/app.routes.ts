01eb0881e24c98e3687a730e5b1a57f7f67b36ea
Ziadsharkos <ZiadElSharkawi@outlook.com>
2025-10-13 20:18:10 -0400

    refactor(router): lazy-load new page containers and keep guard on /todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,31 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login';
-import { RegisterComponent } from './components/register/register';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { 
-    path: 'todos', 
-    loadComponent: () => import('./components/todo-list/todo-list').then(m => m.TodoListComponent),
-    canActivate: [authGuard]
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/pages/login-page/login-page').then(
+        (m) => m.LoginPageComponent
+      ),
   },
-  { path: '**', redirectTo: '/login' }
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./components/pages/register-page/register-page').then(
+        (m) => m.RegisterPageComponent
+      ),
+  },
+  {
+    path: 'todos',
+    loadComponent: () =>
+      import('./components/pages/todos-page/todos-page').then(
+        (m) => m.TodosPageComponent
+      ),
+    canActivate: [authGuard],
+  },
+
+  { path: '**', redirectTo: '/login' },
 ];
